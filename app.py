624f6b004a2cc21da9f05cfbdf72601a4e0a5c69
NickPalmieri7 <143113384+NickPalmieri7@users.noreply.github.com>
2025-08-04 14:16:19 -0400

    Make app compatible with Render port binding

--- app.py
+++ app.py
@@ -62,4 +62,7 @@ def login():
                            severity=severity)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
